37d6ac73d92a6a3b9e8913a5867b8ca6a90f6971
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2021-08-12 14:00:51 +0200

    test_verletlist.py::test_neighbours()

--- tests/et_md3/verletlist/test_verletlist.py
+++ tests/et_md3/verletlist/test_verletlist.py
@@ -33,46 +33,29 @@ def test_vl2pairs():
         print(vl)
         print(et_md3.verletlist.vl2set(vl))
 
-# def test_build_simple_2b():
-#     """"""
-#     x = np.array([0.0, 1, 2, 3, 4])
-#     n_atoms = len(x)
-#     r = np.empty((n_atoms,3))
-#     msg = ['x00','0x0','00x']
-#     for ir in range(3):
-#         r[:,:] = 0.0
-#         r[:,ir] = x
-#         print(f'*** {msg[ir]} ***')
-#
-#         VerletList = et_md3.verletlist.implementation(impl='py')
-#         vl = VerletList(cutoff=2.0)
-#         vl.build_simple(r, keep2d=True)
-#         # print(vl)
-#         pairs = et_md3.verletlist.vl2set(vl)
-#         expected = {(0,1),(0,2),(1,2),(1,3),(2,3),(2,4),(3,4)}
-#         assert pairs == expected
-#
-# def test_neighbours():
-#     """"""
-#     x = np.array([0.0, 1, 2, 3, 4])
-#     n_atoms = len(x)
-#     r = np.empty((n_atoms,3))
-#     msg = ['x00','0x0','00x']
-#     for ir in range(3):
-#         r[:,:] = 0.0
-#         r[:,ir] = x
-#         print(f'*** {msg[ir]} ***')
-#
-#         VerletList = et_md3.verletlist.implementation(impl='py')
-#         vl = VerletList(cutoff=2.0)
-#         vl.build_simple(r)
-#         print(vl.vl_size[0])
-#         assert vl.vl_size[0] == 2
-#         vl0 = vl.verlet_list(0)
-#         print(vl0)
-#         assert vl0[0] == 1
-#         assert vl0[1] == 2
-#
+
+def test_neighbours():
+    """"""
+    x = np.array([0.0, 1, 2, 3, 4])
+    n_atoms = len(x)
+    r = np.empty((n_atoms,3))
+    msg = ['x00','0x0','00x']
+    for ir in range(3):
+        r[:,:] = 0.0
+        r[:,ir] = x
+        print(f'*** {msg[ir]} ***')
+
+        VerletList = et_md3.verletlist.implementation(impl='py')
+        vl = VerletList(cutoff=2.0)
+        build_simple(vl, r)
+        print(vl.vl_size[0])
+        assert vl.vl_size[0] == 2
+        vl0 = vl.verlet_list(0)
+        print(vl0)
+        assert vl0[0] == 1
+        assert vl0[1] == 2
+
+
 # def test_build_1():
 #     """Verify VerletList.build against VerletList.build_simple."""
 #
@@ -120,7 +103,7 @@ def test_vl2pairs():
 
 # ==============================================================================
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_vl2pairs
+    the_test_you_want_to_debug = test_neighbours
 
     print(f'__main__ running {the_test_you_want_to_debug}')
     the_test_you_want_to_debug()
--- tests/et_md3/verletlist/vlbuilders/test_vlbuilders.py
+++ tests/et_md3/verletlist/vlbuilders/test_vlbuilders.py
@@ -91,12 +91,32 @@ def test_build_simple_2():
         assert np.all(vl.vl_list == np.array([1,2,2,3,3,4,4]))
 
 
+def test_build_simple_2b():
+    """"""
+    x = np.array([0.0, 1, 2, 3, 4])
+    n_atoms = len(x)
+    r = np.empty((n_atoms,3))
+    msg = ['x00','0x0','00x']
+    for ir in range(3):
+        r[:,:] = 0.0
+        r[:,ir] = x
+        print(f'*** {msg[ir]} ***')
+
+        VerletList = et_md3.verletlist.implementation(impl='py')
+        vl = VerletList(cutoff=2.0)
+        et_md3.verletlist.vlbuilders.build_simple(vl, r, keep2d=True)
+        # print(vl)
+        pairs = et_md3.verletlist.vl2set(vl)
+        expected = {(0,1),(0,2),(1,2),(1,3),(2,3),(2,4),(3,4)}
+        assert pairs == expected
+
+
 # ==============================================================================
 # The code below is for debugging a particular test in eclipse/pydev.
 # (normally all tests are run with pytest)
 # ==============================================================================
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_build_simple_2
+    the_test_you_want_to_debug = test_build_simple_2b
 
     the_test_you_want_to_debug()
     print("-*# finished #*-")
